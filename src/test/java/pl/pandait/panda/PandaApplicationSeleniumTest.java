b4e83323bf54ba1d26ba7b7594787c79f4c71107
panda <anna.pe@panda.com>
2020-05-28 00:34:39 +0200

    l03

--- src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
+++ src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
@@ -2,6 +2,8 @@ package pl.pandait.panda;
 
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
